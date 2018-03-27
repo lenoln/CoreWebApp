a17fc9f5f651f66d2a927a967d6394bfb95ea7ba
Manoel Pereira <leonamleite@gmail.com>
2018-03-26 22:20:52 -0300

    Base project

---
+++ LibraryData/Models/Friend.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryData.Models
+{
+    public class Friend
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Phone { get; set; }
+    }
+}
